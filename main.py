e52e8a93236a6a5025c162a69ba6b1769058f6e0
Taydon <2889837@student.dublinisc.com>
2024-10-25 15:48:58 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+f = int(input("How many friends do you have?"))
+c = int(input("How many chocolates do you have for you friends?"))
+
+
+print("each friend can have",int(c/f),"chocolates")
+print(c%f,"chocolates left after sharing")
